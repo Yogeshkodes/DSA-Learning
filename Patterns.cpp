fdd827d7c7f28513cac8dca5d24d0878db7c23e7
Yogesh Yadav <122866024+Yogeshkodes@users.noreply.github.com>
2024-03-11 20:15:32 +0530

    Update Patterns.cpp

--- Patterns.cpp
+++ Patterns.cpp
@@ -6,6 +6,41 @@ int main() {
   cin >> num;
 
   /*
+
+    for (int row = 0; row < n; row++) {
+      for (int col = 0; col < n - row; col++) {
+        if (col == 0 || col == n - row - 1) {
+          cout << "*";
+        } else {
+          if (row == 0 || row == n - 1) {
+            cout << "*";
+          } else {
+            cout << " ";
+          }
+        }
+      }
+      cout << endl;
+    }
+  */
+  /*
+    for (int row = 0; row < n; row++) {
+      for (int col = 0; col < 2 * row + 1; col++) {
+        if(col % 2 == 1 ){
+          cout << "*";
+        }else{
+          cout << row + 1;
+        }
+
+       }
+      cout << endl;
+      }
+  */
+
+
+
+
+
+  /*
   int n;
   cin >> n;
 
