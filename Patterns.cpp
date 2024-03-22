caef963c302baed7ceb640bafc870357b9ca0c77
Yogeshkodes <yogesh.cr712@gmail.com>
2024-03-22 21:59:44 +0530

    learned about 2d array and 2d vector

--- Patterns.cpp
+++ Patterns.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 int main() {
-  int num;
-  cin >> num;
 
-  /*
+    int n; 
+    cin >> n ;
+ 
 
     for (int row = 0; row < n; row++) {
       for (int col = 0; col < n - row; col++) {
@@ -19,9 +19,10 @@ int main() {
           }
         }
       }
-      cout << endl;
+      cout << endl;10
+      
     }
-  */
+  
   /*
     for (int row = 0; row < n; row++) {
       for (int col = 0; col < 2 * row + 1; col++) {
