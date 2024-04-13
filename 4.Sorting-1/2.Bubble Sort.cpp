db82fa157fda398ee8e125e7ce93a0edb52b9105
Yogeshkodes <yogesh.cr712@gmail.com>
2024-04-13 20:21:27 +0530

    bubble sort

---
+++ 4.Sorting-1/2.Bubble Sort.cpp
@@ -0,0 +1,23 @@
+void selectionSort(vector<int> &v) {
+  int n = v.size();
+  for (int i = 0; i < n - 1; i++) {
+    int minIndex = i; // ith element hi smallest hai
+    for (int j = i + 1; j < n; j++) {
+      if (v[j] < v[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    // swap ith and minIndex elements;
+    swap(v[i], v[minIndex]);
+  }
+}
+
+
+int main() {
+  vector<int> v = {44,33,55,22,11};
+   bubbleSort(v);
+  
+  print(v);
+  return 0;
+}
