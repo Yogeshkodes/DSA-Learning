c4676235a32513d1476cc54373371b9f2e9b0b8b
Yogeshkodes <yogesh.cr712@gmail.com>
2024-04-13 20:24:34 +0530

    insertion update

---
+++ 4.Sorting-1/3.insertion sort.cpp
@@ -0,0 +1,26 @@
+void insertionSort(vector<int> &v) {
+  int n = v.size();
+  // i = 0, chhod deta hu
+  for (int i = 1; i < n; ++i) {
+    int key = v[i];
+    int j = i - 1;
+
+    // Move elements of arr[0..i-1] that are greater than
+    // key to one position ahead of theri current position
+    while (j >= 0 && v[j] > key) {
+      v[j + 1] = v[j];
+      j--;
+    }
+    v[j + 1] = key; // insertion
+  }
+}
+
+int main() {
+  vector<int> v = {44,33,55,22,11};
+  // bubbleSort(v);
+  // selectionSort(v);
+  insertionSort(v);
+  print(v);
+  return 0;
+}
+
