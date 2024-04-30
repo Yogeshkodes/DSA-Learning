eedc5cf91eda9358578aa8fa0ca5af3f9443038e
Yogeshkodes <yogesh.cr712@gmail.com>
2024-04-30 12:16:47 +0530

    continue

--- 5.Char Array String/Char array string class2.cpp
+++ 5.Char Array String/Char array string class2.cpp
@@ -20,3 +20,14 @@ public:
         }
 };
 
+//* 1910. Remove All Occurrences of a Substring
+
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        while(s.find(part)!= string::npos){
+            s.erase(s.find(part) ,part.length());
+        }
+        return s;
+    }
+};
